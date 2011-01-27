bccd08f7fb012c5a9913212e6af1bff31d192d65
Jon Pierce <jonpierce@gmail.com>
2011-01-27 15:47:38 -0500

    Made code a bit more resilient to potential changes to Twitter.com, customized the dialog title, and incremented the version number in preparation for a new release

--- classic-retweet.js
+++ classic-retweet.js
@@ -1,45 +1,50 @@
-var initClassicRetweet = function()
-{
-  $("head").append($("<style>.tweet-actions .classic-retweet-action span i { background-position:-176px 0px; } .tweet-actions .classic-retweet-action:hover span i { background-position-x:-192px; }</style>"));
+(function() {
+  
+  var initClassicRetweet = function()
+  {
+    $("#page-container").delegate(".tweet", "mouseover", function() {
+      var processed = $(this).data("classic-retweet-processed");
+      if (processed != "true") {
+        $(this).data("classic-retweet-processed", "true");        
+        var retweetAction = $(this).find(".retweet-action");
+        if (retweetAction.size() > 0) {
+          var classicRetweetAction = retweetAction.clone();
+          classicRetweetAction.html(classicRetweetAction.html().replace(/Retweet/, "Classic RT"));
+          retweetAction.after(classicRetweetAction);
+          classicRetweetAction.click(function(event) {
+            var tweet = $(this).closest(".tweet");
+            var content = "RT @" + tweet.attr("data-screen-name") + ": " + tweet.find(".tweet-text").text();
+            new twttr.widget.TweetDialog({
+              basic: false,
+              modal: false,
+              draggable: true,
+              template: {
+                  title: "Classic Retweet"
+              },
+              defaultContent: content,
+              origin: "new-tweet-titlebar-button"
+            }).open().focus();
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+          });
+        }
+      }
+    });  
+  };
 
-  $("#page-container").delegate(".tweet", "mouseover", function() {
-    var addedAttr = "data-classic-retweet-action";
-    var added = $(this).attr(addedAttr);
-    if (added != "true") {
-      $(this).attr(addedAttr, "true");
-      var retweetAction = $(this).find(".retweet-action");
-      var classicRetweetAction = $("<a href=\"#\" class=\"classic-retweet-action\"><span><i></i><b>Classic RT</b></span></a>");
-      retweetAction.after(classicRetweetAction);
-      classicRetweetAction.click(function(event) {
-        var tweet = $(this).closest(".tweet");
-        var content = "RT @" + tweet.attr("data-screen-name") + ": " + tweet.find(".tweet-text").text();
-        new twttr.widget.TweetDialog({
-          modal: false,
-          draggable: true,
-          template: {
-            title: _("What's happening?")
-          },
-          defaultContent: content,
-          origin: "new-tweet-titlebar-button"
-        }).open().focus();
-        event.preventDefault();
-        event.stopPropagation();
-        return false;
-      });
+  var initClassicRetweetAfterJQuery = function()
+  {
+    if (typeof jQuery == "undefined")
+    {
+      setTimeout(initClassicRetweetAfterJQuery, 100);
     }
-  });  
-};
+    else
+    {
+      initClassicRetweet();
+    }
+  };
 
-var initClassicRetweetAfterJQuery = function()
-{
-  if (typeof jQuery == "undefined")
-  {
-    setTimeout(initClassicRetweetAfterJQuery, 100);
-  }
-  else
-  {
-    initClassicRetweet();
-  }
-};
-
-initClassicRetweetAfterJQuery();
+  initClassicRetweetAfterJQuery();
+    
+})();
