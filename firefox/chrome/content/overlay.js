975b210445cf7538b93681d4e5f2ba35bfaac3f6
Jon Pierce <jonpierce@gmail.com>
2011-02-03 01:20:14 -0500

    Tweaked to meet Mozilla's review process requirements

--- firefox/chrome/content/overlay.js
+++ firefox/chrome/content/overlay.js
@@ -1,19 +1,5 @@
 var classicretweet = {
   
-  getContents: function(url) {
-    var ioService = Components.classes["@mozilla.org/network/io-service;1"]
-      .getService(Components.interfaces.nsIIOService);
-    var scriptableStream = Components.classes["@mozilla.org/scriptableinputstream;1"]
-      .getService(Components.interfaces.nsIScriptableInputStream);
-    var channel = ioService.newChannel(url, null, null);
-    var input = channel.open();
-    scriptableStream.init(input);
-    var contents = scriptableStream.read(input.available());
-    scriptableStream.close();
-    input.close();    
-    return contents;
-  },
-  
   onLoad: function() {
     var appcontent = document.getElementById("appcontent"); // if browser
     if (appcontent) {
@@ -25,11 +11,17 @@ var classicretweet = {
     var doc = event.originalTarget; // doc is document that triggered "onload" event
     if (doc.location.href.match(/http:\/\/twitter.com|https:\/\/twitter.com/)) {
       var scriptUrl = "chrome://classicretweet/content/classic-retweet.js";
-      var scriptContents = classicretweet.getContents(scriptUrl);
-      var script = doc.createElement("script");
-      script.type = "text/javascript";
-      script.innerHTML = scriptContents;
-      doc.getElementsByTagName("body")[0].appendChild(script);
+      var req = new XMLHttpRequest();
+      req.onload = function() {
+        var scriptContents = req.responseText;
+        var script = doc.createElement("script");
+        script.type = "text/javascript";
+        script.textContent = scriptContents;
+        doc.getElementsByTagName("body")[0].appendChild(script);        
+      };
+      req.open("GET", scriptUrl, true);      
+      req.overrideMimeType("text/plain"); // otherwise will attempt to parse as xml
+      req.send(null);
     }    
   }
 
